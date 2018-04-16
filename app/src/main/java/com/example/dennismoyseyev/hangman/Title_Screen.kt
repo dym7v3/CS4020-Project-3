7af91b5f2565916c04d808542c68d6c16f2d9a6a
Dennis Moyseyev <denomoyseyev@gmail.com>
2018-04-16 17:12:11 -0500

    Initial commit

---
+++ app/src/main/java/com/example/dennismoyseyev/hangman/Title_Screen.kt
@@ -0,0 +1,12 @@
+package com.example.dennismoyseyev.hangman
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Title_Screen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_title__screen)
+    }
+}
