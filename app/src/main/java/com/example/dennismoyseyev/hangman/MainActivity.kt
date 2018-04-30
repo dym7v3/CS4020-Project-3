7b8b6cee33742bade763bb43b59e456798deadb2
Dennis Moyseyev <denomoyseyev@gmail.com>
2018-04-30 17:25:05 -0500

    "Have the keyboard working. When the user presses enter then it will call a function. Now I need to build the text processes function. I also need to do the drawables for the hangman. "

--- app/src/main/java/com/example/dennismoyseyev/hangman/MainActivity.kt
+++ app/src/main/java/com/example/dennismoyseyev/hangman/MainActivity.kt
@@ -4,17 +4,40 @@ import android.app.Activity
 import android.content.pm.ActivityInfo
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.view.KeyEvent
+import android.view.inputmethod.EditorInfo
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
+//The data model.
 var model: gameModel = gameModel()
+
+
 class MainActivity : Activity() {
+    lateinit var field: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
         setContentView(R.layout.activity_main)
         my_pharse.text = String(model.char_arry)
+        field = findViewById(R.id.input_text) as EditText
+    }
 
+
+
+    override fun onResume() {
+        super.onResume()
+        field.setOnEditorActionListener( { textView, action, event ->
+            var handled = false
+            if (action == EditorInfo.IME_ACTION_DONE) {
+                Toast.makeText(getApplicationContext(), "Your toast message.", Toast.LENGTH_SHORT).show()
+            }
+            handled
+        })
     }
 
 }
--- app/src/main/java/com/example/dennismoyseyev/hangman/gameModel.kt
+++ app/src/main/java/com/example/dennismoyseyev/hangman/gameModel.kt
@@ -1,16 +1,20 @@
 package com.example.dennismoyseyev.hangman
 import java.util.Random
 
+//The data model.
 class gameModel{
     val random : Random
     val phrase_num: Int
     var phrase: String
     var char_arry: CharArray
+    val orginal_phrase_arry: CharArray
 
+    //The initializing block of code.
     init {
         random = Random()
         phrase_num = rand(1, 10)
         phrase = which_phrase(phrase_num)
+        orginal_phrase_arry = phrase.toCharArray()
         char_arry = phrase.toCharArray()
         process_text()
     }
@@ -20,6 +24,7 @@ class gameModel{
         return random.nextInt(to - from) + from
     }
 
+    //This function runs when the game is started and it randomly selects a phrase.
     private fun which_phrase(num: Int) : String {
         return when (num) {
             1 -> "memory like an elephant"
@@ -37,16 +42,18 @@ class gameModel{
             }
         }
     }
+    //This covers the letters of the text.
     private fun process_text()
     {
         for (i in char_arry.indices)
         {
             if (char_arry.get(i).equals(' '))
             {
-
+                //Does nothing because this is already a space in the phrase.
             }
             else
             {
+                //This will switch the letter for the actually leetter to a ~ to hide the text.
                 char_arry.set(i, '~')
             }
         }
